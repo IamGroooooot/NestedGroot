7ec35ad45354dba03789aeed7e4a86b2d22f434c
IamGroooooot <dury.ko@gmail.com>
2019-05-01 13:24:09 +0900

    .

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  siteTitle: "GrootBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "GrootBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "GrootBlog is a GatsbyJS starter.",
+  siteTitle: "GrootBlog - Studying Computer Science", // <title>
+  shortSiteTitle: "GrootBlog - Studying Computer Science", // <title> ending for posts and pages
+  siteDescription: "GrootBlog is for my Studying Footprintf.",
   siteUrl: "https://iamgroooooot.github.io/GrootPage/index.html",
   pathPrefix: '/GrootPage',
   siteImage: "preview.jpg",
   siteLanguage: "en",
 
   /* author */
-  authorName: "groot",
-  authorTwitterAccount: "iamgroot",
+  authorName: "Juhyeong Ko",
+  authorTwitterAccount: "juhyeongko",
 
   /* info */
-  headerTitle: "groot",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "Juhyeong Ko",
+  headerSubTitle: "CAU Student",
 
   /* manifest.json */
   manifestName: "GrootBlog - a GatsbyJS starter",
@@ -28,12 +28,12 @@ module.exports = {
   // Replace your email adress with md5-code.
   // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
   // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
-  gravatarImgMd5: "",
+  gravatarImgMd5: "https://ko.gravatar.com/avatar/22eb20dd1ddc03eea77d7a7f9dae7ead",
 
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/iamgroooooot" },
+    { name: "twitter", url: "https://twitter.com/juhyeongko" },
+    { name: "facebook", url: "http://facebook.com/Ko.juhyeong" }
   ]
 };
